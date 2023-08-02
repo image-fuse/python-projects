f07ed698a563079734ce89356fe9fbb38cfe5051
image-fuse <image@fusemachines.com>
2023-08-02 22:03:54 +0545

    feat: added docstring and type hints to functions

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ def add(x, y):
     return x + y
 
 def subtract(x, y):
+    '''
+    Returns the difference between two user input numbers.
+    '''
     return x - y
 
 def multiply(x, y):
+    '''
+    Returns the product of two user input numbers.
+    '''
     return x * y
 
 def divide(x, y):
+    '''
+    Returns the quotient when one of two user input numbers is divided by the other.
+    '''
     if y != 0:
         return x / y
     else:
