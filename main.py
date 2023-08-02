a8887bb4f6657d082751f4235702af1f0586990d
image-fuse <image@fusemachines.com>
2023-08-02 17:32:54 +0545

    feat: functions for add, sub and mult

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def subtract(x, y):
 def multiply(x, y):
     return x * y
 
-def divide(x, y):
-    if y != 0:
-        return x / y
-    else:
-        raise ValueError("Cannot divide by zero")
+# def divide(x, y):
+#     if y != 0:
+#         return x / y
+#     else:
+#         raise ValueError("Cannot divide by zero")
